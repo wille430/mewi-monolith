6ab74b5f41b21eda14d9f291f209ecb44537111f
William W <wille0403@gmail.com>
2021-12-20 09:21:35 +0100

    fix: watcher API CRUD

--- packages/webapp/src/api/watcherAPI.ts
+++ packages/webapp/src/api/watcherAPI.ts
@@ -1,22 +1,37 @@
-import { JWT } from '@mewi/types'
 import axios from 'axios'
+import { ElasticQuery, WatcherMetadata } from 'types/types'
 
 const { NX_API_URL } = process.env
 
-export const getWatchers = async (token: JWT) => {
+export const getWatchers = async () => {
     const watchers = await axios.get(NX_API_URL + '/user/watchers').then(res => res.data)
     return watchers
 }
 
-export const getWatcher = async (token: JWT, watcherId: string) => {
+export const getWatcher = async (watcherId: string) => {
     const watcher = await axios.get(NX_API_URL + '/user/watchers/' + watcherId).then(res => res.data)
     return watcher
 }
 
-export const deleteWatcher = async (token: JWT, watcherId: string) => {
+export const deleteWatcher = async (watcherId: string) => {
     await axios.delete(NX_API_URL + '/user/watchers/' + watcherId)
 }
 
-export const createWatcher = async (token: JWT, {metadata, query}) => {
-    await axios.post(NX_API_URL + '/user/watchers')
+interface createWatcherProps {
+    metadata: WatcherMetadata,
+    query: ElasticQuery
+}
+
+export const createWatcher = async ({ metadata, query }: createWatcherProps) => {
+    const newWatcher = await axios.post(NX_API_URL + '/user/watchers', { metadata, query })
+    return newWatcher
+}
+
+interface updateWatcherProps extends createWatcherProps {
+
+}
+
+export const updateWatcher = async (watcherId: string, { metadata, query }: updateWatcherProps) => {
+    const newWatcher = await axios.patch(NX_API_URL + '/user/watchers' + watcherId, { metadata, query })
+    return newWatcher
 }
