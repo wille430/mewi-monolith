7d698b4d1f8bc498e376ea3d040af47112d9e163
William W <wille0403@gmail.com>
2021-12-21 22:34:36 +0100

    fix: working on e2e testing

---
+++ packages/webapp-e2e/src/integration/watchers.spec.ts
@@ -0,0 +1,25 @@
+
+describe('watchers', () => {
+    cy.fixture('api').as('apiData')
+
+    let accessToken
+
+    beforeEach(() => {
+        cy.request('post', 'http://localhost:3001/test/user').then(res => {
+            accessToken = res.body.jwt
+        })
+        cy.visit('/minabevakningar')
+    })
+
+    it('it can create a new watcher and display it', () => {
+
+        cy.get('[data-testid=createNewWatcherButton]').click()
+
+        cy.get('[data-testid=keywordInput]').type('hello world')
+
+        cy.get('[data-testid=sendButton]').click()
+
+        cy.contains('hello world')
+        
+    })
+})
