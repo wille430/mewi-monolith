d9f8c56d6910896851c86ae6fa26e7499216f08a
William Desktop <41762983+wille430@users.noreply.github.com>
2021-12-22 16:07:27 +0100

    fix: cross platform startup script

--- tools/start_local_dev_env.sh
+++ tools/start_local_dev_env.sh
@@ -1,14 +1,30 @@
 #!bin/bash
-cd $(dirname $0)
-set -e
-cleanup() {
-    echo "Stopping mongodb..."
-    sudo systemctl stop mongodb
-}
 
-echo Starting mongodb service...
-sudo systemctl start mongodb
-echo starting elasticsearch...
-./elasticsearch-7.16.1/bin/elasticsearch
+select opt in linux windows; do
+    case $opt in
+        linux)
+            echo "Starting dev env for linux..."
+            cd $(dirname $0)
+            set -e
+            cleanup() {
+                echo "Stopping mongodb..."
+                sudo systemctl stop mongodb
+            }
 
-trap cleanup EXIT
+            echo Starting mongodb service...
+            sudo systemctl start mongodb
+
+            echo starting elasticsearch...
+            ./elasticsearch-7.16.1/bin/elasticsearch
+
+            trap cleanup EXIT
+            ;;
+        windows)
+            echo "Starting dev env for windows..."
+            ./elasticsearch-7.16.2/bin/elasticsearch.bat
+            ;;
+        *)
+            echo "Invalid option $REPLY"
+            ;;
+    esac
+done
