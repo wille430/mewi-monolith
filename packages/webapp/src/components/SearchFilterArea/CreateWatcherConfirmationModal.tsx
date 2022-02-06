81994c510608815fd25f223c270d0c61e747fc47
William Desktop <41762983+wille430@users.noreply.github.com>
2022-02-06 15:17:26 +0100

    style: no pointer events

--- packages/webapp/src/components/SearchFilterArea/CreateWatcherConfirmationModal.tsx
+++ packages/webapp/src/components/SearchFilterArea/CreateWatcherConfirmationModal.tsx
@@ -14,7 +14,7 @@ const CreateWatcherConfirmationModal = ({
 }: CreateWatcherConfirmationModalProps) => {
     return (
         <PopUp show={Boolean(open)} onOutsideClick={onExit}>
-            <div className='jusitfy-center flex h-3/4 w-full items-center'>
+            <div className='jusitfy-center flex h-3/4 w-full items-center pointer-events-none'>
                 <Modal
                     heading='Är du säker att du vill lägga en bevakning på sökningen?'
                     bodyText='Genom att klicka på "Ja" godkänner du att ta emot e-post varje gång det kommer nya föremål som matchar din sökning.'
