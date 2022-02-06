b3cee13a6bfccba7b5058c612eecb0c4b9016c18
William Desktop <41762983+wille430@users.noreply.github.com>
2022-02-06 16:34:02 +0100

    fix: pointer events in modal

--- packages/webapp/src/components/SearchFilterArea/CreateWatcherConfirmationModal.tsx
+++ packages/webapp/src/components/SearchFilterArea/CreateWatcherConfirmationModal.tsx
@@ -14,15 +14,22 @@ const CreateWatcherConfirmationModal = ({
 }: CreateWatcherConfirmationModalProps) => {
     return (
         <PopUp show={Boolean(open)} onOutsideClick={onExit}>
-            <div className='jusitfy-center flex h-3/4 w-full items-center pointer-events-none'>
-                <Modal
-                    heading='Är du säker att du vill lägga en bevakning på sökningen?'
-                    bodyText='Genom att klicka på "Ja" godkänner du att ta emot e-post varje gång det kommer nya föremål som matchar din sökning.'
-                    actions={[
-                        <Button label='Nej' variant='text' onClick={onExit} data-testid='modalDecline' />,
-                        <Button label='Ja' onClick={onAccept} data-testid='modalAccept' />,
-                    ]}
-                />
+            <div className='jusitfy-center pointer-events-none flex h-3/4 w-full items-center'>
+                <div className='pointer-events-auto mx-auto'>
+                    <Modal
+                        heading='Är du säker att du vill lägga en bevakning på sökningen?'
+                        bodyText='Genom att klicka på "Ja" godkänner du att ta emot e-post varje gång det kommer nya föremål som matchar din sökning.'
+                        actions={[
+                            <Button
+                                label='Nej'
+                                variant='text'
+                                onClick={onExit}
+                                data-testid='modalDecline'
+                            />,
+                            <Button label='Ja' onClick={onAccept} data-testid='modalAccept' />,
+                        ]}
+                    />
+                </div>
             </div>
         </PopUp>
     )
