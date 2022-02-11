0cc7c30ffb4019bfa24c9a26bbe71e7af34e2510
William Desktop <41762983+wille430@users.noreply.github.com>
2022-02-11 14:59:53 +0100

    fix: display description

--- packages/webapp/src/components/ArticleItemDetails/Description/index.tsx
+++ packages/webapp/src/components/ArticleItemDetails/Description/index.tsx
@@ -1,12 +1,13 @@
+
 const Description = ({ text }: { text: string }) => {
     const paragraphs = text.split('\n')
 
     return (
-        <>
-            {paragraphs.forEach((paragraph) => (
+        <div className='space-y-6'>
+            {paragraphs.map((paragraph) => (
                 <p>{paragraph}</p>
             ))}
-        </>
+        </div>
     )
 }
 
