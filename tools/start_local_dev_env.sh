0eb303378d418265003db82e15b07040ed5912fd
wille430 <41762983+wille430@users.noreply.github.com>
2022-03-06 19:16:30 +0100

    fix: kill ports before start

--- tools/start_local_dev_env.sh
+++ tools/start_local_dev_env.sh
@@ -14,6 +14,7 @@ select opt in linux windows elasticsearch; do
             ;;
         windows)
             echo "Starting dev env for windows..."
+            npx kill-port 4200 3001 9200
             ./tools/elasticsearch-7.16.2/bin/elasticsearch.bat & cd ../ & npx nx serve api & npx nx serve webapp
 
 
