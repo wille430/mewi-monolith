74cf3c3560095db71a1ea2cc41bf0f9b92eeccfd
wille430 <41762983+wille430@users.noreply.github.com>
2022-03-06 21:19:48 +0100

    feat: ubuntu server bootstrap script

---
+++ tools/bootstrap_server.sh
@@ -0,0 +1,21 @@
+# INSTALL MONGODB
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org=5.0.6 mongodb-org-database=5.0.6 mongodb-org-server=5.0.6 mongodb-org-shell=5.0.6 mongodb-org-mongos=5.0.6 mongodb-org-tools=5.0.6
+
+# INSTALL ELASTICSEARCH
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt update
+sudo apt install elasticsearch
+
+
+
+# START SERVICES
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+sudo systemctl start elasticsearch
+sudo systemctl enable elasticsearch
