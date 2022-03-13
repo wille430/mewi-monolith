67db8a66e37708f44930752683dcdbebb9000218
wille430 <41762983+wille430@users.noreply.github.com>
2022-03-13 15:44:29 +0100

    fix: linting

--- packages/webapp/src/components/PopUp/PopUp.tsx
+++ packages/webapp/src/components/PopUp/PopUp.tsx
@@ -30,7 +30,7 @@ const PopUp = ({ children, show = true, onOutsideClick, className, ...rest }: Po
                 <motion.div
                     {...rest}
                     className={classNames({
-                        ['fixed top-0 left-0 z-40 h-screen w-screen overflow-y-scroll bg-black bg-opacity-40 p-4']:
+                        'fixed top-0 left-0 z-40 h-screen w-screen overflow-y-scroll bg-black bg-opacity-40 p-4':
                             true,
                         [className || '']: !!className,
                     })}
