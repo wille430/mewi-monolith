6c27028cfc6be4cb626cc601177e0c653e2c1477
wille430 <41762983+wille430@users.noreply.github.com>
2022-03-14 19:08:50 +0100

    test: forgotten password page

--- packages/webapp/src/components/StyledLoader.js
+++ packages/webapp/src/components/StyledLoader.js
@@ -2,7 +2,7 @@ import Loader from 'react-loader-spinner'
 import theme from 'themes/theme'
 
 const StyledLoader = () => {
-    return <Loader type='TailSpin' height='40px' width='40px' color={theme.accent1} />
+    return <Loader type='TailSpin' height='40px' width='40px' color={theme.accent1} data-testid='spinner' />
 }
 
 export default StyledLoader
