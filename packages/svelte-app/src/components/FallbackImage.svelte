9368a21b0159e3022857cea7ee351d379d55692c
William W <wille0403@gmail.com>
2022-05-04 21:55:53 +0200

    feat: svelte app

---
+++ packages/svelte-app/src/components/FallbackImage.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import fallback from '../assets/missingImage.png';
+
+	export let src: string;
+	export let alt: string | undefined;
+
+	const handleError = (e: any) => {
+		e.target.src = fallback;
+	};
+</script>
+
+<img {src} {alt} on:error={handleError} />
+
+<style>
+	img {
+		width: 100%;
+		height: auto;
+	}
+</style>
---
+++ packages/svelte-app/src/components/SearchInput.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	let query = '';
+</script>
+
+<div class="form-group d-flex flex-column">
+	<form action="/sok" class="input-group">
+		<input
+			class="form-control"
+			type="text"
+			name="query"
+			bind:value={query}
+			placeholder="Vad letar du efter?"
+		/>
+		<input class="btn btn-secondary" type="submit" value="Sök" />
+	</form>
+
+	<a class="text-secondary small text-end mt-2" href="/filter">Advancerade filter >></a>
+</div>
+
+<style lang="scss">
+	.input-group {
+		width: 24rem;
+		max-width: 100%;
+	}
+</style>
