911eb18b633abda33c4861012d48f4a75a7e1d03
William W <wille0403@gmail.com>
2022-05-15 19:09:31 +0200

    fix: deployment issue

--- tools/copy_packages.sh
+++ tools/copy_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PACKAGE_PATH="./node_modules/@mewi/prisma/"
+PACKAGE_PATH="./node_modules/@mewi/prisma/*"
 for dir in ./packages/*; do
-    mkdir $dir/node_modules/@mewi/prisma
+    mkdir -p $dir/node_modules/@mewi/prisma
     cp $PACKAGE_PATH $dir/node_modules/@mewi/prisma/ -r
 done
