90cc57a2b6e78139f4dac52582f2ee16acee3600
William W <wille0403@gmail.com>
2022-06-14 16:52:37 +0200

    fix: add provider

--- packages/nest-api/src/scrapers/scrapers.ts
+++ packages/nest-api/src/scrapers/scrapers.ts
@@ -1,5 +1,6 @@
 import { BlippScraper } from './scrapers/blipp.scraper'
 import { BlocketScraper } from './scrapers/blocket.scraper'
+import { BytbilScraper } from './scrapers/bytbil.scraper'
 import { CitiboardScraper } from './scrapers/citiboard.scraper'
 import { SellpyScraper } from './scrapers/sellpy.scraper'
 import { ShpockScraper } from './scrapers/shpock.scraper'
@@ -12,6 +13,7 @@ const Scrapers = [
     BlocketScraper,
     CitiboardScraper,
     ShpockScraper,
+    BytbilScraper,
 ]
 
 export default Scrapers
