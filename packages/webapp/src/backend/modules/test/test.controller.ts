4bc6084d6c0532d70b7def1880077c84295c5f8c
William Wigemo <william.wigemo@outlook.com>
2022-11-01 18:27:31 +0100

    fdasdassa

--- packages/webapp/src/backend/modules/test/test.controller.ts
+++ packages/webapp/src/backend/modules/test/test.controller.ts
@@ -1,14 +1,11 @@
-import { Controller, Get, Res } from '@nestjs/common'
-import type { Response } from 'express'
 import type { TestService } from './test.service'
-import { setJWTCookies } from '@/auth/utils/setJWTCookies'
 
 @Controller('/test')
 export class TestController {
     constructor(private testService: TestService) {}
 
     @Get('/user')
-    async getUser(@Res({ passthrough: true }) res: Response) {
+    async getUser(@Res({ passthrough: true }) res: any) {
         const accessTokens = await this.testService.createUser()
 
         setJWTCookies(res, accessTokens)
