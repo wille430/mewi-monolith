b3962578388f61f97d6085602aece7865d1f2f98
William Wigemo <william.wigemo@outlook.com>
2022-11-13 19:09:46 +0100

    feat: analyze script

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
-const flowRight = require('lodash/flowRight')
+const flowRight = require('lodash/flowRight');
 
-const funcs = []
-
-if (process.env.NODE_ENV === 'development') {
-    funcs.push(
-        require('@next/bundle-analyzer')({
-            enabled: process.env.ANALYZE === 'true',
-        })
-    )
-}
+const funcs = [
+    require('@next/bundle-analyzer')({
+        enabled: process.env.ANALYZE === 'true',
+    })
+]
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
