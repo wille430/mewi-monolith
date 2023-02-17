80b4ef8d4aae8fff5ea353607effd04f43e15c45
William Wigemo <william.wigemo@outlook.com>
2023-02-17 11:55:25 +0100

    fix: remove footer arch

--- apps/next-client/src/lib/components/Footer/Footer.tsx
+++ apps/next-client/src/lib/components/Footer/Footer.tsx
@@ -1,55 +1,49 @@
-import { Category , CategoryLabel } from '@/common/schemas'
+import {Category, CategoryLabel} from '@/common/schemas'
 import Link from 'next/link'
 import styles from './Footer.module.scss'
-import { Arch } from '../Arch/Arch'
 import clsx from "clsx"
 
 export const Footer = () => {
     return (
-        <div>
-            <div className='rotate-180 pb-2'>
-                <Arch />
+        <footer className={styles.footer}>
+            <div className={clsx(styles['inner-footer'], "container px-2")}>
+                <ul className={styles['link-list']}>
+                    <li>
+                        <Link href="/">Hem</Link>
+                    </li>
+                    <li>
+                        <Link href="/loggain">Logga in</Link>
+                    </li>
+                    <li>
+                        <Link href="/nyttkonto">Skapa ett konto</Link>
+                    </li>
+                </ul>
+                <ul className={styles['link-list']}>
+                    <li>
+                        <Link href="/kategorier">Alla kategorier</Link>
+                    </li>
+                    <ul className="grid gap-3 md:grid-cols-2">
+                        {Object.keys(Category).map((key) => (
+                            <li key={key} className="text-sm">
+                                <Link href={`/sok?categories=/${key}`}>
+                                    {CategoryLabel[key as Category]}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </ul>
+                <ul className={styles['link-list']}>
+                    <li>
+                        <Link href="/minasidor/bevakningar">Mina sidor</Link>
+                    </li>
+                    <li>
+                        <Link href="/minasidor/bevakningar">Bevakningar</Link>
+                    </li>
+                    <li>
+                        <Link href="/minasidor/konto">Konto</Link>
+                    </li>
+                </ul>
             </div>
-            <footer className={styles.footer}>
-                <div className={clsx(styles['inner-footer'], "container")}>
-                    <ul className={styles['link-list']}>
-                        <li>
-                            <Link href='/'>Hem</Link>
-                        </li>
-                        <li>
-                            <Link href='/loggain'>Logga in</Link>
-                        </li>
-                        <li>
-                            <Link href='/nyttkonto'>Skapa ett konto</Link>
-                        </li>
-                    </ul>
-                    <ul className={styles['link-list']}>
-                        <li>
-                            <Link href='/kategorier'>Alla kategorier</Link>
-                        </li>
-                        <ul className='grid gap-3 md:grid-cols-2'>
-                            {Object.keys(Category).map((key) => (
-                                <li key={key} className='text-sm'>
-                                    <Link href={`/sok?categories=/${key}`}>
-                                        {CategoryLabel[key as Category]}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </ul>
-                    <ul className={styles['link-list']}>
-                        <li>
-                            <Link href='/minasidor/bevakningar'>Mina sidor</Link>
-                        </li>
-                        <li>
-                            <Link href='/minasidor/bevakningar'>Bevakningar</Link>
-                        </li>
-                        <li>
-                            <Link href='/minasidor/konto'>Konto</Link>
-                        </li>
-                    </ul>
-                </div>
-            </footer>
-        </div>
+        </footer>
     )
 }
