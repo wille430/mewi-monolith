674449274ba5a43c0023e140839c2d780b74a5a7
William Wigemo <william.wigemo@outlook.com>
2023-02-26 16:23:26 +0100

    fix: request Sellpy listings with SEK price

--- apps/scrape-service/src/scrapers/Sellpy/SellpyEndPoint.ts
+++ apps/scrape-service/src/scrapers/Sellpy/SellpyEndPoint.ts
@@ -1,45 +1,52 @@
-import type {AxiosResponse} from 'axios'
-import {ListingParser} from '../ListingParser'
-import {ScrapeMetadata} from '../Scraper'
-import {AbstractEndPoint} from "../EndPoint"
-import {Category, Currency, ListingOrigin} from "@mewi/models"
-import {Listing} from "@mewi/entities"
-import {SellpyListing} from "./SellpyListing.interface"
-import axios from "axios"
+import type {AxiosResponse} from "axios";
+import axios from "axios";
+import {ListingParser} from "../ListingParser";
+import {ScrapeMetadata} from "../Scraper";
+import {AbstractEndPoint} from "../EndPoint";
+import {Category, Currency, ListingOrigin} from "@mewi/models";
+import {Listing} from "@mewi/entities";
+import {SellpyListing} from "./SellpyListing.interface";
 
-export class SellpyEndPoint extends AbstractEndPoint<Listing, AxiosResponse, SellpyListing> {
-    private limit = 50
-    private parser: ListingParser = new ListingParser(ListingOrigin.Sellpy, this)
+export class SellpyEndPoint extends AbstractEndPoint<
+    Listing,
+    AxiosResponse,
+    SellpyListing
+> {
+    private limit = 50;
+    private parser: ListingParser = new ListingParser(ListingOrigin.Sellpy, this);
 
     constructor() {
-        super(SellpyEndPoint.name)
+        super(SellpyEndPoint.name);
     }
 
     protected getResponse(page: number): Promise<AxiosResponse> {
         return axios({
-            method: 'POST',
-            url: `https://m6wnfr0lvi-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(4.8.0)%3B%20Browser%20(lite)%3B%20JS%20Helper%20(3.2.2)%3B%20react%20(16.13.1)%3B%20react-instantsearch%20(6.7.0)&x-algolia-api-key=313e09c3b00b6e2da5dbe382cd1c8f4b&x-algolia-application-id=M6WNFR0LVI`,
+            method: "POST",
+            url: `https://m6wnfr0lvi-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(4.14.2)%3B%20Browser%20(lite)%3B%20JS%20Helper%20(3.2.2)%3B%20react%20(16.13.1)%3B%20react-instantsearch%20(6.7.0)&x-algolia-api-key=313e09c3b00b6e2da5dbe382cd1c8f4b&x-algolia-application-id=M6WNFR0LVI`,
             data: {
                 requests: [
                     {
-                        indexName: 'prod_marketItem_en_relevance',
+                        indexName: "prod_marketItem_se_relevance",
                         params: `highlightPreTag=%3Cais-highlight-0000000000%3E&highlightPostTag=%3C%2Fais-highlight-0000000000%3E&hitsPerPage=${
                             this.limit
-                        }&filters=NOT%20private%20%3D%201%20AND%20price_EU.amount%20%3E%200&clickAnalytics=true&maxValuesPerFacet=10000&query=&page=${
+                        }&filters=price_SE.amount%20%3E%200%20AND%20(NOT%20access_SE%3Afirst%20OR%20saleType%3Ainfluencer)&clickAnalytics=true&analyticsTags=%5B%22WEB%22%2C%22Search%22%2C%22NotLoggedIn%22%5D&maxValuesPerFacet=2000&query=&page=${
                             page - 1
-                        }&facets=%5B%22promo%22%2C%22user%22%2C%22campaign%22%2C%22objectID%22%2C%22featuredIn%22%2C%22lastChance%22%2C%22metadata.demography%22%2C%22sizes%22%2C%22metadata.brand%22%2C%22metadata.type%22%2C%22metadata.condition%22%2C%22pricing.amount%22%2C%22metadata.color%22%2C%22metadata.material%22%2C%22metadata.pattern%22%2C%22metadata.sleeveLength%22%2C%22metadata.neckline%22%2C%22metadata.restrictedModel%22%2C%22categories.lvl0%22%5D&tagFilters=`,
+                        }&facets=%5B%22sizes%22%2C%22metadata.brand%22%2C%22metadata.type%22%2C%22metadata.demography%22%2C%22metadata.condition%22%2C%22storageSite%22%2C%22metadata.color%22%2C%22metadata.material%22%2C%22metadata.fabric%22%2C%22metadata.pattern%22%2C%22metadata.neckline%22%2C%22metadata.sleeveLength%22%2C%22metadata.garmentLength%22%2C%22metadata.waistRise%22%2C%22metadata.restrictedModel%22%2C%22salesChannel%22%2C%22pricing.amount%22%2C%22inHmResell_SE%22%2C%22inHmResell_DE%22%2C%22promo%22%2C%22user%22%2C%22campaign%22%2C%22objectID%22%2C%22featuredIn%22%2C%22lastChance%22%2C%22categories.lvl0%22%5D&tagFilters=`,
                     },
                 ],
             },
-        })
+        });
     }
 
-    protected extractEntities(res: AxiosResponse): SellpyListing[] | Promise<SellpyListing[]> {
-        return res.data.results[0].hits
+    protected extractEntities(
+        res: AxiosResponse
+    ): SellpyListing[] | Promise<SellpyListing[]> {
+        return res.data.results[0].hits;
     }
 
-
-    protected parseRawEntity(obj: any): Partial<Listing> | Promise<Partial<Listing>> {
+    protected parseRawEntity(
+        obj: any
+    ): Partial<Listing> | Promise<Partial<Listing>> {
         return this.parser.parseListing({
             origin_id: this.parser.createId(obj.objectID),
             title: obj.metadata.brand ?? obj.metadata.type,
@@ -55,12 +62,12 @@ export class SellpyEndPoint extends AbstractEndPoint<Listing, AxiosResponse, Sel
                     currency: Currency.SEK,
                 }
                 : undefined,
-        })
+        });
     }
 
     protected getScrapeMetadata(res: AxiosResponse): Promise<ScrapeMetadata> {
         return Promise.resolve({
             totalPages: res.data.results[0].nbPages,
-        })
+        });
     }
 }
--- apps/scrape-service/src/scrapers/tests/EndPoint.spec.ts
+++ apps/scrape-service/src/scrapers/tests/EndPoint.spec.ts
@@ -1,18 +1,19 @@
-import {Listing} from "@mewi/entities"
-import {ScrapeResponse} from "../Scraper"
-import {validate} from "class-validator"
-import {plainToInstance} from "class-transformer"
-import {ListingValidator} from "../../models/ListingValidator"
-import {AbstractEndPoint} from "../EndPoint"
-import {BilwebScraper} from "../Bilweb/BilwebScraper"
-import {BlippScraper} from "../Blipp/BlippScraper"
-import {BytbilScraper} from "../Bytbil/BytbilScraper"
-import {BlocketScraper} from "../Blocket/BlocketScraper"
-import {CitiboardScraper} from "../Citiboard/CitiboardScraper"
-import {KvdBilScraper} from "../KvdBil/KvdBilScraper"
-import {SellpyScraper} from "../Sellpy/SellpyScraper"
-import {ShpockScraper} from "../Shpock/ShpockScraper"
-import {TraderaScraper} from "../Tradera/TraderaScraper"
+import {Listing} from "@mewi/entities";
+import {ScrapeResponse} from "../Scraper";
+import {validate} from "class-validator";
+import {plainToInstance} from "class-transformer";
+import {ListingValidator} from "../../models/ListingValidator";
+import {AbstractEndPoint} from "../EndPoint";
+import {BilwebScraper} from "../Bilweb/BilwebScraper";
+import {BlippScraper} from "../Blipp/BlippScraper";
+import {BytbilScraper} from "../Bytbil/BytbilScraper";
+import {BlocketScraper} from "../Blocket/BlocketScraper";
+import {CitiboardScraper} from "../Citiboard/CitiboardScraper";
+import {KvdBilScraper} from "../KvdBil/KvdBilScraper";
+import {SellpyScraper} from "../Sellpy/SellpyScraper";
+import {ShpockScraper} from "../Shpock/ShpockScraper";
+import {TraderaScraper} from "../Tradera/TraderaScraper";
+import {expect} from "vitest";
 
 describe.each([
     ...BilwebScraper.createEndpoints(),
@@ -23,35 +24,35 @@ describe.each([
     ...KvdBilScraper.createEndpoints(),
     ...SellpyScraper.createEndpoints(),
     ...ShpockScraper.createEndpoints(),
-    ...TraderaScraper.createEndpoints()
+    ...TraderaScraper.createEndpoints(),
 ])("%s", (endpoint: AbstractEndPoint<any, any, any>) => {
-
-    const scrapeAmount = 10
+    const scrapeAmount = 10;
 
     describe("#scrape", () => {
-        let res: ScrapeResponse<Listing>
+        let res: ScrapeResponse<Listing>;
         beforeEach(async () => {
-            res = await endpoint.scrape(scrapeAmount)
-        })
+            res = await endpoint.scrape(scrapeAmount);
+        });
 
         it("should return an array of listings", async () => {
-            expect(Array.isArray(res.entities)).toBe(true)
-            expect(res.entities.length).toBe(scrapeAmount)
+            expect(Array.isArray(res.entities)).toBe(true);
+            expect(res.entities.length).toBe(scrapeAmount);
 
-            let invalidListings = 0
-            let hasNoImage = 0
+            let invalidListings = 0;
+            let hasNoImage = 0;
             for (const listing of res.entities) {
-                const err = await validate(plainToInstance(ListingValidator, listing))
+                const err = await validate(plainToInstance(ListingValidator, listing));
                 if (err.length) {
-                    console.error(err)
-                    invalidListings++
+                    console.error(err);
+                    invalidListings++;
                 }
 
-                if (listing.imageUrl.length == 0) hasNoImage++
+                if (listing.imageUrl.length == 0) hasNoImage++;
+                console.log(`${listing.redirectUrl} - ${listing.price.value}`);
             }
-            expect(invalidListings).toBe(0)
+            expect(invalidListings).toBe(0);
             // Some listings must at least have an image
-            expect(hasNoImage).toBeLessThan(res.entities.length)
-        })
-    })
-})
+            expect(hasNoImage).toBeLessThan(res.entities.length);
+        });
+    });
+});
