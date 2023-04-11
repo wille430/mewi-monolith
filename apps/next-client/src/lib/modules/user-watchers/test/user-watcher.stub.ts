701b41fcc70eafda67a3d6ff223a26408ffdf3c6
William Wigemo <william.wigemo@outlook.com>
2023-04-11 15:07:44 +0200

    fix: type error

--- apps/next-client/src/lib/modules/user-watchers/test/user-watcher.stub.ts
+++ apps/next-client/src/lib/modules/user-watchers/test/user-watcher.stub.ts
@@ -1,24 +1,33 @@
-import {timestampsStub} from "@/test/stubs/timestamps.stub";
-import type {WithId} from "@/lib/types/utils";
+import { timestampsStub } from "@/test/stubs/timestamps.stub";
+import type { WithId } from "@/lib/types/utils";
 import mongoose from "mongoose";
-import {UserWatcher} from "@mewi/entities";
-import {userStub} from "../../users/test/stubs/user.stub";
-import {watcherStub} from "../../watchers/test/stubs/watcher.stub";
+import { UserWatcher } from "@mewi/entities";
+import { userStub } from "../../users/test/stubs/user.stub";
+import { watcherStub } from "../../watchers/test/stubs/watcher.stub";
 
 const id = "6336ec2f0fce785ab7fb1d31";
-export const userWatcherStub = (): WithId<UserWatcher> => ({
+export const userWatcherStub = (): WithId<UserWatcher> => {
+  const userWatcher = new UserWatcher();
+
+  Object.assign(userWatcher, {
     _id: new mongoose.Types.ObjectId(id) as any,
     id,
     user: userStub() as any,
     watcher: watcherStub() as any,
     ...timestampsStub(),
-});
+  });
 
-export const createUserWatcherStub = (): Omit<WithId<UserWatcher>, "user" | "watcher"> & {
-    user: any
-    watcher: any
+  return userWatcher;
+};
+
+export const createUserWatcherStub = (): Omit<
+  WithId<UserWatcher>,
+  "user" | "watcher"
+> & {
+  user: any;
+  watcher: any;
 } => ({
-    ...userWatcherStub(),
-    user: userStub()._id,
-    watcher: watcherStub()._id,
+  ...userWatcherStub(),
+  user: userStub()._id,
+  watcher: watcherStub()._id,
 });
